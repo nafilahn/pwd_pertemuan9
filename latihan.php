4bb9a2d440d3f3099cb79c4bb4f2619c9399f371
nafilah <nafilahnursabila443@gmail.com>
2022-05-30 08:58:52 +0700

    menambahkan file baru

---
+++ latihan.php
@@ -0,0 +1,25 @@
+<!-- php : server side (mengelola)-->
+<!-- cara menulis syntax PHP-->
+<?php
+//menampilkan data ke layar
+// echo "Hello World";
+var_dump("Hello World");
+
+//membuat variabel
+//1. diawali dg simbol dollar
+//2. tdk boleh diawali dg angka & tdk ada spasi (alt: underscore, namaMhs)
+$nama = "Nafilah";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba PHP</title>
+</head>
+<body>
+    
+</body>
+</html>
